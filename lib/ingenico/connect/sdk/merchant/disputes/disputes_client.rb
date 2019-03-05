0d20f2728e2c5af21b7920ab861d3fee759f8da9
Ingenico ePayments <jenkins@isaac.nl>
2019-03-05 16:17:36 +0100

    Release 1.32.0.

--- lib/ingenico/connect/sdk/merchant/disputes/disputes_client.rb
+++ lib/ingenico/connect/sdk/merchant/disputes/disputes_client.rb
@@ -80,6 +80,37 @@ module Ingenico::Connect::SDK
           error_object = @communicator.marshaller.unmarshal(e.body, error_type)
           raise create_exception(e.status_code, e.body, error_object, context)
         end
+
+        # Resource /{{merchantId}}/disputes/{{disputeId}}/cancel - {https://epayments-api.developer-ingenico.com/s2sapi/v1/en_US/ruby/disputes/cancel.html Cancel dispute}
+        # dispute_id:: String
+        # context::    {Ingenico::Connect::SDK::CallContext}
+        # Returns:: {Ingenico::Connect::SDK::Domain::Dispute::DisputeResponse}
+        # Raises:: {Ingenico::Connect::SDK::ValidationException} if the request was not correct and couldn't be processed (HTTP status code 400)
+        # Raises:: {Ingenico::Connect::SDK::AuthorizationException} if the request was not allowed (HTTP status code 403)
+        # Raises:: {Ingenico::Connect::SDK::IdempotenceException} if an idempotent request caused a conflict (HTTP status code 409)
+        # Raises:: {Ingenico::Connect::SDK::ReferenceException} if an object was attempted to be referenced that doesn't exist or has been removed,  
+        #          or there was a conflict (HTTP status code 404, 409 or 410)
+        # Raises:: {Ingenico::Connect::SDK::GlobalCollectException} if something went wrong at the Ingenico ePayments platform,  
+        #          the Ingenico ePayments platform was unable to process a message from a downstream partner/acquirer,  
+        #          or the service that you're trying to reach is temporary unavailable (HTTP status code 500, 502 or 503)
+        # Raises:: {Ingenico::Connect::SDK::ApiException} if the Ingenico ePayments platform returned any other error
+        def cancel(dispute_id, context=nil)
+          path_context = {
+            'disputeId' => dispute_id,
+          }
+          uri = instantiate_uri('/{apiVersion}/{merchantId}/disputes/{disputeId}/cancel', path_context)
+          return @communicator.post(
+            uri,
+            client_headers,
+            nil,
+            nil,
+            Ingenico::Connect::SDK::Domain::Dispute::DisputeResponse,
+            context)
+        rescue ResponseException => e
+          error_type = Ingenico::Connect::SDK::Domain::Errors::ErrorResponse
+          error_object = @communicator.marshaller.unmarshal(e.body, error_type)
+          raise create_exception(e.status_code, e.body, error_object, context)
+        end
       end
     end
   end
