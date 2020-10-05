44c1c9088f3fd40467a9658f82a1d7a2765fa061
Ingenico ePayments <github@epay.ingenico.com>
2020-10-05 16:30:46 +0200

    Release 2.17.0.

--- lib/ingenico/connect/sdk/domain/hostedcheckout/mobile_payment_method_specific_input_hosted_checkout.rb
+++ lib/ingenico/connect/sdk/domain/hostedcheckout/mobile_payment_method_specific_input_hosted_checkout.rb
@@ -10,24 +10,46 @@ module Ingenico::Connect::SDK
   module Domain
     module Hostedcheckout
 
+      # @attr [String] authorization_mode
+      # @attr [String] customer_reference
       # @attr [Ingenico::Connect::SDK::Domain::Hostedcheckout::MobilePaymentProduct302SpecificInputHostedCheckout] payment_product302_specific_input
       # @attr [Ingenico::Connect::SDK::Domain::Hostedcheckout::MobilePaymentProduct320SpecificInputHostedCheckout] payment_product320_specific_input
+      # @attr [true/false] requires_approval
+      # @attr [true/false] skip_fraud_service
       class MobilePaymentMethodSpecificInputHostedCheckout < Ingenico::Connect::SDK::Domain::Definitions::AbstractPaymentMethodSpecificInput
+
+        attr_accessor :authorization_mode
+
+        attr_accessor :customer_reference
 
         attr_accessor :payment_product302_specific_input
 
         attr_accessor :payment_product320_specific_input
 
+        attr_accessor :requires_approval
+
+        attr_accessor :skip_fraud_service
+
         # @return (Hash)
         def to_h
           hash = super
+          hash['authorizationMode'] = @authorization_mode unless @authorization_mode.nil?
+          hash['customerReference'] = @customer_reference unless @customer_reference.nil?
           hash['paymentProduct302SpecificInput'] = @payment_product302_specific_input.to_h unless @payment_product302_specific_input.nil?
           hash['paymentProduct320SpecificInput'] = @payment_product320_specific_input.to_h unless @payment_product320_specific_input.nil?
+          hash['requiresApproval'] = @requires_approval unless @requires_approval.nil?
+          hash['skipFraudService'] = @skip_fraud_service unless @skip_fraud_service.nil?
           hash
         end
 
         def from_hash(hash)
           super
+          if hash.has_key? 'authorizationMode'
+            @authorization_mode = hash['authorizationMode']
+          end
+          if hash.has_key? 'customerReference'
+            @customer_reference = hash['customerReference']
+          end
           if hash.has_key? 'paymentProduct302SpecificInput'
             raise TypeError, "value '%s' is not a Hash" % [hash['paymentProduct302SpecificInput']] unless hash['paymentProduct302SpecificInput'].is_a? Hash
             @payment_product302_specific_input = Ingenico::Connect::SDK::Domain::Hostedcheckout::MobilePaymentProduct302SpecificInputHostedCheckout.new_from_hash(hash['paymentProduct302SpecificInput'])
@@ -35,6 +57,12 @@ module Ingenico::Connect::SDK
           if hash.has_key? 'paymentProduct320SpecificInput'
             raise TypeError, "value '%s' is not a Hash" % [hash['paymentProduct320SpecificInput']] unless hash['paymentProduct320SpecificInput'].is_a? Hash
             @payment_product320_specific_input = Ingenico::Connect::SDK::Domain::Hostedcheckout::MobilePaymentProduct320SpecificInputHostedCheckout.new_from_hash(hash['paymentProduct320SpecificInput'])
+          end
+          if hash.has_key? 'requiresApproval'
+            @requires_approval = hash['requiresApproval']
+          end
+          if hash.has_key? 'skipFraudService'
+            @skip_fraud_service = hash['skipFraudService']
           end
         end
       end
--- lib/ingenico/connect/sdk/domain/payment/mobile_payment_product320_specific_input.rb
+++ lib/ingenico/connect/sdk/domain/payment/mobile_payment_product320_specific_input.rb
@@ -7,36 +7,22 @@ require 'ingenico/connect/sdk/domain/payment/g_pay_three_d_secure'
 
 module Ingenico::Connect::SDK
   module Domain
-    module Hostedcheckout
+    module Payment
 
-      # @attr [String] merchant_name
-      # @attr [String] merchant_origin
       # @attr [Ingenico::Connect::SDK::Domain::Payment::GPayThreeDSecure] three_d_secure
-      class MobilePaymentProduct320SpecificInputHostedCheckout < Ingenico::Connect::SDK::DataObject
-
-        attr_accessor :merchant_name
-
-        attr_accessor :merchant_origin
+      class MobilePaymentProduct320SpecificInput < Ingenico::Connect::SDK::DataObject
 
         attr_accessor :three_d_secure
 
         # @return (Hash)
         def to_h
           hash = super
-          hash['merchantName'] = @merchant_name unless @merchant_name.nil?
-          hash['merchantOrigin'] = @merchant_origin unless @merchant_origin.nil?
           hash['threeDSecure'] = @three_d_secure.to_h unless @three_d_secure.nil?
           hash
         end
 
         def from_hash(hash)
           super
-          if hash.has_key? 'merchantName'
-            @merchant_name = hash['merchantName']
-          end
-          if hash.has_key? 'merchantOrigin'
-            @merchant_origin = hash['merchantOrigin']
-          end
           if hash.has_key? 'threeDSecure'
             raise TypeError, "value '%s' is not a Hash" % [hash['threeDSecure']] unless hash['threeDSecure'].is_a? Hash
             @three_d_secure = Ingenico::Connect::SDK::Domain::Payment::GPayThreeDSecure.new_from_hash(hash['threeDSecure'])
