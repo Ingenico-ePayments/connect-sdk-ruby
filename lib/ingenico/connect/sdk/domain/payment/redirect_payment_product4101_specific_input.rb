ae86744af6b4639dfb3465ffc4cf0d431ea328c4
Ingenico ePayments <github@epay.ingenico.com>
2021-08-03 17:05:07 +0200

    Release 2.24.0.

--- lib/ingenico/connect/sdk/domain/payment/redirect_payment_product4101_specific_input.rb
+++ lib/ingenico/connect/sdk/domain/payment/redirect_payment_product4101_specific_input.rb
@@ -8,43 +8,36 @@ module Ingenico::Connect::SDK
   module Domain
     module Payment
 
+      # @attr [String] display_name
       # @attr [String] integration_type
-      # @attr [String] merchant_name
-      # @attr [String] transaction_note
-      # @attr [String] vpa
+      # @attr [String] virtual_payment_address
       class RedirectPaymentProduct4101SpecificInput < Ingenico::Connect::SDK::DataObject
+
+        attr_accessor :display_name
 
         attr_accessor :integration_type
 
-        attr_accessor :merchant_name
-
-        attr_accessor :transaction_note
-
-        attr_accessor :vpa
+        attr_accessor :virtual_payment_address
 
         # @return (Hash)
         def to_h
           hash = super
+          hash['displayName'] = @display_name unless @display_name.nil?
           hash['integrationType'] = @integration_type unless @integration_type.nil?
-          hash['merchantName'] = @merchant_name unless @merchant_name.nil?
-          hash['transactionNote'] = @transaction_note unless @transaction_note.nil?
-          hash['vpa'] = @vpa unless @vpa.nil?
+          hash['virtualPaymentAddress'] = @virtual_payment_address unless @virtual_payment_address.nil?
           hash
         end
 
         def from_hash(hash)
           super
+          if hash.has_key? 'displayName'
+            @display_name = hash['displayName']
+          end
           if hash.has_key? 'integrationType'
             @integration_type = hash['integrationType']
           end
-          if hash.has_key? 'merchantName'
-            @merchant_name = hash['merchantName']
-          end
-          if hash.has_key? 'transactionNote'
-            @transaction_note = hash['transactionNote']
-          end
-          if hash.has_key? 'vpa'
-            @vpa = hash['vpa']
+          if hash.has_key? 'virtualPaymentAddress'
+            @virtual_payment_address = hash['virtualPaymentAddress']
           end
         end
       end
