d96f3bf22f77113d0fae3d6f364886a9afe730e8
Ingenico ePayments <github@epay.ingenico.com>
2022-10-06 09:47:47 +0200

    Release 2.33.0.

--- lib/ingenico/connect/sdk/domain/definitions/microsoft_fraud_results.rb
+++ lib/ingenico/connect/sdk/domain/definitions/microsoft_fraud_results.rb
@@ -8,22 +8,50 @@ module Ingenico::Connect::SDK
   module Domain
     module Definitions
 
+      # @attr [String] device_country_code
+      # @attr [String] device_id
       # @attr [Integer] fraud_score
+      # @attr [String] true_ip_address
+      # @attr [String] user_device_type
       class MicrosoftFraudResults < Ingenico::Connect::SDK::DataObject
 
+        attr_accessor :device_country_code
+
+        attr_accessor :device_id
+
         attr_accessor :fraud_score
+
+        attr_accessor :true_ip_address
+
+        attr_accessor :user_device_type
 
         # @return (Hash)
         def to_h
           hash = super
+          hash['deviceCountryCode'] = @device_country_code unless @device_country_code.nil?
+          hash['deviceId'] = @device_id unless @device_id.nil?
           hash['fraudScore'] = @fraud_score unless @fraud_score.nil?
+          hash['trueIpAddress'] = @true_ip_address unless @true_ip_address.nil?
+          hash['userDeviceType'] = @user_device_type unless @user_device_type.nil?
           hash
         end
 
         def from_hash(hash)
           super
+          if hash.has_key? 'deviceCountryCode'
+            @device_country_code = hash['deviceCountryCode']
+          end
+          if hash.has_key? 'deviceId'
+            @device_id = hash['deviceId']
+          end
           if hash.has_key? 'fraudScore'
             @fraud_score = hash['fraudScore']
+          end
+          if hash.has_key? 'trueIpAddress'
+            @true_ip_address = hash['trueIpAddress']
+          end
+          if hash.has_key? 'userDeviceType'
+            @user_device_type = hash['userDeviceType']
           end
         end
       end
