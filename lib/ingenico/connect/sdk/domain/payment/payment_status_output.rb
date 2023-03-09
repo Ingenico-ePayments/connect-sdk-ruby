169a3d08a255a177eb3332266a58bddb02047a08
Ingenico ePayments <github@epay.ingenico.com>
2023-03-09 08:48:49 +0100

    Release 2.36.0.

--- lib/ingenico/connect/sdk/domain/payment/payment_status_output.rb
+++ lib/ingenico/connect/sdk/domain/payment/payment_status_output.rb
@@ -10,12 +10,15 @@ module Ingenico::Connect::SDK
 
       # @attr [true/false] is_authorized
       # @attr [true/false] is_refundable
+      # @attr [true/false] is_retriable
       # @attr [String] three_d_secure_status
       class PaymentStatusOutput < Ingenico::Connect::SDK::Domain::Definitions::OrderStatusOutput
 
         attr_accessor :is_authorized
 
         attr_accessor :is_refundable
+
+        attr_accessor :is_retriable
 
         attr_accessor :three_d_secure_status
 
@@ -24,6 +27,7 @@ module Ingenico::Connect::SDK
           hash = super
           hash['isAuthorized'] = @is_authorized unless @is_authorized.nil?
           hash['isRefundable'] = @is_refundable unless @is_refundable.nil?
+          hash['isRetriable'] = @is_retriable unless @is_retriable.nil?
           hash['threeDSecureStatus'] = @three_d_secure_status unless @three_d_secure_status.nil?
           hash
         end
@@ -35,6 +39,9 @@ module Ingenico::Connect::SDK
           end
           if hash.has_key? 'isRefundable'
             @is_refundable = hash['isRefundable']
+          end
+          if hash.has_key? 'isRetriable'
+            @is_retriable = hash['isRetriable']
           end
           if hash.has_key? 'threeDSecureStatus'
             @three_d_secure_status = hash['threeDSecureStatus']
