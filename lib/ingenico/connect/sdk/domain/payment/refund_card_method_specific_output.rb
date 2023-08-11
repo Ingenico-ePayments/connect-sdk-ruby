241efb2bf4250ee2ac87a2fe8100a7051b3925a5
Ingenico ePayments <github@epay.ingenico.com>
2023-08-11 09:42:05 +0200

    Release 2.42.0.

--- lib/ingenico/connect/sdk/domain/payment/refund_card_method_specific_output.rb
+++ lib/ingenico/connect/sdk/domain/payment/refund_card_method_specific_output.rb
@@ -2,6 +2,7 @@
 # This class was auto-generated from the API references found at
 # https://epayments-api.developer-ingenico.com/s2sapi/v1/
 #
+require 'ingenico/connect/sdk/domain/definitions/card_essentials'
 require 'ingenico/connect/sdk/domain/payment/refund_method_specific_output'
 
 module Ingenico::Connect::SDK
@@ -9,14 +10,18 @@ module Ingenico::Connect::SDK
     module Payment
 
       # @attr [String] authorisation_code
+      # @attr [Ingenico::Connect::SDK::Domain::Definitions::CardEssentials] card
       class RefundCardMethodSpecificOutput < Ingenico::Connect::SDK::Domain::Payment::RefundMethodSpecificOutput
 
         attr_accessor :authorisation_code
+
+        attr_accessor :card
 
         # @return (Hash)
         def to_h
           hash = super
           hash['authorisationCode'] = @authorisation_code unless @authorisation_code.nil?
+          hash['card'] = @card.to_h unless @card.nil?
           hash
         end
 
@@ -24,6 +29,10 @@ module Ingenico::Connect::SDK
           super
           if hash.has_key? 'authorisationCode'
             @authorisation_code = hash['authorisationCode']
+          end
+          if hash.has_key? 'card'
+            raise TypeError, "value '%s' is not a Hash" % [hash['card']] unless hash['card'].is_a? Hash
+            @card = Ingenico::Connect::SDK::Domain::Definitions::CardEssentials.new_from_hash(hash['card'])
           end
         end
       end
